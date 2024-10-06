95b3ec567691dd5b713278d0282e783ddc3c27a1
Keilwerth11270 <fahims.dida@gmail.com>
2024-10-05 21:08:59 -0400

    Initial commit: Trying Rust for the first time

---
+++ hello_world/target/debug/deps/hello_world-eb808b3e177280eb.d
@@ -0,0 +1,5 @@
+/home/fahim/Hello_Rust/hello_world/target/debug/deps/hello_world-eb808b3e177280eb: src/main.rs
+
+/home/fahim/Hello_Rust/hello_world/target/debug/deps/hello_world-eb808b3e177280eb.d: src/main.rs
+
+src/main.rs:
---
+++ hello_world/target/debug/hello_world.d
@@ -0,0 +1,1 @@
+/home/fahim/Hello_Rust/hello_world/target/debug/hello_world: /home/fahim/Hello_Rust/hello_world/src/main.rs
